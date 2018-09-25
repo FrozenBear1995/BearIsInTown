736e6a517ed44537f2429e66f59f0b0ab87dd578
Xiong Lin <xlin85@student.monash.edu>
2018-09-25 19:03:55 +1000

    Initial Commit

---
+++ BearIsInTown/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BearIsInTown
+//
+//  Created by Xiong Lin on 25/9/18.
+//  Copyright © 2018 Xiong Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
